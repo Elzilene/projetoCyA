16513dabb8a48470fa3dcd921fdd5cd01fef9eea
Elzilene <lenesoares.souza@gmail.com>
2023-09-06 11:31:05 -0300

    adicionado o comando customizado de validação de contrato

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,4 +1,6 @@
 import Ajv from 'ajv'
+import {definitionHelper} from '../utils/schemaDefinitions'
+
 Cypress.Commands.add('login',(email, password) =>{
     cy.request({
         method:'POST',
@@ -14,13 +16,27 @@ Cypress.Commands.add('login',(email, password) =>{
         })
     })
 })
-Cypress.Commands.add('testeContrato',()=>{
+Cypress.Commands.add('testeContrato',(schema, resposta)=>{
     // Função de mostrar os erros
-    const getSchemaErros(ajvErros)=>{
+    const getSchemaError(ajvErros)=>{
         return cy.wrap(
             `campo:${ajvErros[0]['instancePath']} é invalido.Erros:${ajvErros[0]['message']}`
 
         )
 
     }
+    // iniciar o ajv
+    const ajv = new Ajv()
+    const validacao = ajv.addSchema().compile(schema)
+    const valido = validacao.validate(resposta)
+
+    //verificar se o schema passou ou falhou
+    if(!valido){
+        getSchemaError(validacao.errors).then((schemaError)=>{
+            throw new Error(schemaError)
+        })
+    }else{
+        expect(valido, 'validacão de contrato').to.be.true
+    }
+     
 })
